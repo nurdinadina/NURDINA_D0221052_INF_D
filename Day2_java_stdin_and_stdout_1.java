0fd26fc8065e60f2525c3db5790b23620ec78ca4
nurdinadina <99631221+nurdinadina@users.noreply.github.com>
2022-02-20 16:31:43 +0800

    Day2_java_stdin_and_stdout_1.java

---
+++ Day2_java_stdin_and_stdout_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int d = scan.nextInt();
+        int e = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(e);
+        
+        
+    }
+}
